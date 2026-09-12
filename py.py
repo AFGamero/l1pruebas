075cdfa588b052261eff8d76e9e8c231842ef684
gamero <andresgamero28sa@gmail.com>
2026-09-12 13:33:29 -0500

    subiendo ultimas diapos

--- py.py
+++ py.py
@@ -1,10 +1,16 @@
 def sumar(a, y):
     print(f"Sumando {a} y {y}")
-    return a + b
+    return a + y
 
 def restar(a, b):
     return a - b
 
 
 def multiplicar(a,b):
+<<<<<<< Updated upstream
     return a*b
+=======
+    return a*b
+
+# asd
+>>>>>>> Stashed changes
